ec381b94114891d25fb7b8040825d16d0d95dc6e
dhinta <relaxed.dhinta@gmail.com>
2021-04-11 19:39:08 +0530

    Queue with weeve

---
+++ Problem Solving Patterns/queue.js
@@ -0,0 +1,32 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(item = null) {
+    this.data.unshift(item);
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+const weeve = (source1, source2) => {
+  const combined = new Queue();
+
+  while (source1.peek() || source2.peek()) {
+    if (source1.peek()) {
+      combined.add(source1.remove());
+    }
+    if (source2.peek()) {
+      combined.add(source2.remove());
+    }
+  }
+
+  return combined;
+};
