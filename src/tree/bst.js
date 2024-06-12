0c5c6cc96989b6b6d0fd733c42c778d16a6931a4
Debasish <relaxed.dhinta@gmil.com>
2024-06-12 22:55:50 +0530

    bst

---
+++ src/tree/bst.js
@@ -0,0 +1,81 @@
+// Binary search tree
+
+class Node {
+  constructor(val = 0) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Bst {
+  constructor() {
+    this.root = null;
+    this.length = 0;
+    this.depth = 0;
+  }
+
+  insert(val) {
+    const node = new Node(val);
+    if (this.root === null) {
+      this.root = node;
+      return this;
+    }
+
+    let current = null;
+    let next = this.root;
+
+    while (next) {
+      current = next;
+      if (val > next.val) {
+        next = next.right;
+      } else if (val < current.val) {
+        next = next.left;
+      } else {
+        throw new Error(`Value ${val} already exists`);
+      }
+    }
+
+    if (val > current.val) {
+      current.right = node;
+    } else {
+      current.left = node;
+    }
+
+    return this;
+  }
+
+  find(val) {
+    if (this.root === null) {
+      return null;
+    }
+
+    let current = this.root;
+
+    while (current) {
+      if (current.val === val) {
+        return current;
+      }
+
+      if (val > current.val) {
+        current = current.right;
+      } else {
+        current = current.left;
+      }
+    }
+
+    return null;
+  }
+}
+
+/*
+        10
+            \
+            15
+            /   \  
+        14     20
+                /
+            16
+                       
+
+*/
